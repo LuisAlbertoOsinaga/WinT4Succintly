9dd303cea0062fff1ea2bf3a3cfadcf710e00282
Luis Alberto Osinaga <luis.osinaga@gmail.com>
2017-08-09 15:42:04 -0400

    Clase AppSettings generada sin indentación apropiada

--- T4AppSettings/AppSettings.cs
+++ T4AppSettings/AppSettings.cs
@@ -1,1 +1,25 @@
 ﻿
+// Generado por Laos, el 09/08/2017 0:00:00
+
+using System; 
+namespace book 
+{ 
+	class Program 
+	{ 
+		static void Hello(string[] args) 
+		{ 
+			Console.WriteLine("Hello World of T4");
+			Console.WriteLine("Hola Mundo en este 8/9/2017 12:00:00 AM");
+			Console.ReadKey(); 
+		} 
+	}
+
+	public class AppSettings 
+	{ 
+		 public string PreserveLoginUrl{get { return " true";}}
+  public string ClientValidationEnabled{get { return " true";}}
+  public string UnobtrusiveJavaScriptEnabled{get { return " true";}}
+ 
+	}
+}
+
